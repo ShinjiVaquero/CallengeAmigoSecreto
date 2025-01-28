73e6bde31a794250e171ed34833a3a7cb4d7cb9b
ShinjiVaquero <trujillohernandezmustang@yahoo.com.mx>
2025-01-27 22:25:58 -0600

    Creación de la función para ingresar los nombres

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 // Declaración del array de amigos
 let amigos = [];
+
+// Creación de la función para agregar a los amigos
+function agregarAmigo() {
+    nombre = document.getElementById("amigo").value;
+    console.log(nombre);
+}
+// Llamado de las funciones
+agregarAmigo();
